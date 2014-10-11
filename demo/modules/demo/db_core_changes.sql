dafed0d130b219c361d60548bc6362584702e173
Fabian <tethys.framework@qnote.de>
2014-10-11 20:54:19 +0200

    Bugfix Demo Modul Settings

---
+++ demo/modules/demo/db_core_changes.sql
@@ -0,0 +1,7 @@
+
+-- Tethys Core Changes
+
+INSERT INTO `core_settings` (`id` ,`key` ,`modul` ,`user` ,`type` ,`value`)
+VALUES (NULL , 'FEATURE1', 'demo', NULL , 'CHECKBOX', '1' );
+
+-- DB:Fabian -----------------------------------------------------------------
