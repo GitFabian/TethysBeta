a7acbe8ce0b550d2b81d95c1791e0bcd9ddcee7d
Fabian <tethys.framework@qnote.de>
2014-11-15 18:41:36 +0100

    Unlimited Users

--- core/settings.php
+++ core/settings.php
@@ -59,7 +59,7 @@ function setting_get_default($modul,$key){
 		if ($key=='CFG_EDIT_NICK') return "0";
 		if ($key=='CFG_EDIT_FILE') return "1";
 		if ($key=='CFG_HAUPTMENUE') return ROOT_HDD_CORE."\\configExample.php";
-		if ($key=='CFG_MAX_USERS') return "1";
+		if ($key=='CFG_MAX_USERS') return "0";
 		if ($key=='FEATURE_PRERELEASE') return "1";
 		if ($key=='CFG_AUTHPATTERN') return "nachname+\"_\"+vorname";
 		if ($key=='UPDATE_KOMMANDOS') return "cd /d \"".ROOT_HDD_CORE."\" && git pull";
