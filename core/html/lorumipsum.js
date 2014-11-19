14350e88b4c63fa46f97f0c8a656e227bca4eecb
fabian <gitfabian@qnote.de>
2014-11-19 16:27:33 +0100

    lorumipsum für inputfelder

--- core/html/lorumipsum.js
+++ core/html/lorumipsum.js
@@ -14,5 +14,18 @@ function lorumipsum_start(root,ext){
 			};
 			xmlhttp.send();
 		});
+	$('input[type=text]').bind('click', function(event){
+			if(!(event.ctrlKey&&event.shiftKey)) return;
+			e=this;
+			e.value="Lade...";
+			var xmlhttp=new XMLHttpRequest();
+			xmlhttp.open("GET",root+"/core/ajax."+ext+"?cmd=lorumipsum&length=short",true);
+			xmlhttp.onreadystatechange=function(){
+				if (xmlhttp.readyState==4 && xmlhttp.status==200){
+					e.value=xmlhttp.responseText;
+				}
+			};
+			xmlhttp.send();
+		});
 	});
 }
