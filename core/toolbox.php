cbd9439101d3843cf47fc57b26f8ae916dd1cbd7
Fabian <tethys.framework@qnote.de>
2014-11-30 17:50:19 +0100

    html_to_plain

--- core/toolbox.php
+++ core/toolbox.php
@@ -574,4 +574,11 @@ function parse_url_query($url,$key='id'){
 	return $pars[$key];
 }
 
+function html_to_plain($html){
+	$plain=preg_replace("/\\<.*?\\>/s", "", $html);
+	$plain=html_entity_decode($plain);
+	$plain=trim($plain);
+	return $plain;
+}
+
 ?>
