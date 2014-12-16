0c6d78b6da5879ee0b94c02b98c60fd30aca3522
fabian <gitfabian@qnote.de>
2014-12-16 15:12:07 +0100

    nbsp tbl options

--- core/classes/table.php
+++ core/classes/table.php
@@ -216,7 +216,7 @@ class table_row{
 			}
 		}
 		if ($options){
-			$line="\n\t\t\t\t".implode("\n\t\t\t\t", $options)."\n\t\t\t";
+			$line="\n\t\t\t\t".implode("&nbsp;", $options)."\n\t\t\t";
 			preg_match_all("/\\[ID\\:(.*?)\\]/", $line, $matches);
 			for ($i = 0; $i < count($matches[1]); $i++) {
 				$idkey=$matches[1][$i];
