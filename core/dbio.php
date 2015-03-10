8bb308975bebe19d28c33077b543e4a923e26d93
fabian <gitfabian@qnote.de>
2015-03-10 16:20:47 +0100

    return id

--- core/dbio.php
+++ core/dbio.php
@@ -71,7 +71,9 @@ function dbio_NEW_FROM_REQUEST($db,$idkey="id",$unsets=null){
 function dbio_INSERT_LOG($modul,$db,$data,$link_identifier=null){
 	include_once ROOT_HDD_CORE.'/core/log.php';
 	dbio_INSERT($db,$data,$link_identifier);
+	$id=mysql_insert_id();
 	log_db_new2($modul, $db, mysql_insert_id(), $data);
+	return $id;
 }
 
 function dbio_INSERT($db,$data,$link_identifier=null){
