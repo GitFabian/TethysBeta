f6eb965834ccc53ad0bc173c19568c3455ab50dc
fabian <gitfabian@qnote.de>
2015-06-16 15:59:54 +0200

    falschrum

--- core/admin/chronjobs.php
+++ core/admin/chronjobs.php
@@ -4,7 +4,7 @@ $page->init('core_chronjobs','Chronjobs');
 if (!USER_ADMIN) page_send_exit("Keine Berechtigung!");
 include_once ROOT_HDD_CORE.'/core/classes/table.php';
 
-$table=new table(dbio_SELECT("core_chronjobs",null,"*",null,"schedule",true));
+$table=new table(dbio_SELECT("core_chronjobs",null,"*",null,"schedule",false));
 $table->set_options(true, true, true, "core_chronjobs");
 $page->say($table);
 // if($table->rows)
