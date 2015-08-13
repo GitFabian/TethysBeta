1b4eabe6fff8f21e06f18671497c5f56f15d1112
fabian <gitfabian@qnote.de>
2015-08-13 12:47:04 +0200

    gantt:days

--- core/classes/gantt.php
+++ core/classes/gantt.php
@@ -20,12 +20,17 @@ class gantt{
 	var $size_factor;
 	var $start;
 	var $end;
+	var $cols;
 
-	function __construct($size_factor=0.0001654/*1 Woche=100 px*/){
+	/**
+	 * @param string $cols = ["KW"|"DAY"]
+	 */
+	function __construct($size_factor=0.0001654/*1 Woche=100 px*/,$cols="KW"){
 		$this->data=array();
 		$this->size_factor=$size_factor;
 		$this->start=strtotime(date("Y-m-d"));
 		$this->end=$this->start+86400;
+		$this->cols=$cols;
 	}
 	
 	function __toString(){
@@ -35,14 +40,35 @@ class gantt{
 		$begin=strtotime("Monday this week",$this->start-86400);
 		
 		//Überschrift
-		$width=floor(604800/*1 Woche*/*$this->size_factor);
-		$start=$begin;
-		while($start<$this->end){
-			$year=date("M'y",$start+302400/*Mitte der Woche*/);
-			$kw=date("W",$start+302400/*Mitte der Woche*/);
-			$left=floor(($start-$begin)*$this->size_factor);
-			$html.="<div class=\"gantt_head\" style=\"width:{$width}px;left:{$left}px;\"><span class=\"gantt_head_text\">$year, KW$kw</span></div>";
-			$start+=604800/*1 Woche*/;
+		if($this->cols=="KW"){
+			$width=floor(604800/*1 Woche*/*$this->size_factor);
+			$start=$begin;
+			while($start<$this->end){
+				$year=date("M'y",$start+302400/*Mitte der Woche*/);
+				$kw=date("W",$start+302400/*Mitte der Woche*/);
+				$left=floor(($start-$begin)*$this->size_factor);
+				$html.="<div class=\"gantt_head\" style=\"width:{$width}px;left:{$left}px;\"><span class=\"gantt_head_text\">$year, KW$kw</span></div>";
+				$start+=604800/*1 Woche*/;
+			}
+		}
+		if($this->cols=="DAY"){
+// 			$begin-=86400;
+			$monate=array("Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember");
+			$head.="<br>";
+			$width=floor(86400/*1 Tag*/*$this->size_factor);
+			$start=$begin;
+			$monat="";
+			while($start<$this->end){
+				$left=floor(($start-$begin)*$this->size_factor);
+				$day=date("j",$start+1800/*Mitte des Tages*/);
+				$mon=$monate[date("n",$start+1800/*Mitte des Tages*/)-1]."&nbsp;".date("Y",$start+1800/*Mitte des Tages*/);
+				if($mon!=$monat){ $monat=$mon; }else{ $mon=""; }
+				$html.="<div class=\"gantt_head\" style=\"width:{$width}px;left:{$left}px;\">"
+						."<span class=\"gantt_head_text\">$mon<br>$day</span>"
+					."</div>";
+				$start+=86400/*1 Tag*/;
+			}
+			$html.="<br>";
 		}
 		$html.="<br>";
 		
