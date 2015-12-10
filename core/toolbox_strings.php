2b71f1bacc6d91239e285888ed58593c39fa486e
fabian <gitfabian@qnote.de>
2015-12-10 17:41:23 +0100

    tmarkup

---
+++ core/toolbox_strings.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+include_once ROOT_HDD_CORE.'/core/toolbox_strings.php';
+ */
+
+function tmarkup($string){
+	/*
+	 * Link der Form "[DESC](http://URL)" (markdown)
+	 * =============================================
+	 * /  [.*  ]  (http:  /  /.*  )/
+	 * /\\[.*\\]\\(http:\\/\\/.*\\)/ Zeichen escapen
+	 * 
+	 * /\\[.*\\]\\(http:\\/\\/.*\\)/
+	 * /\\[.*\\]\\(http:\\/\\/.*\\)/i Case-insensitive
+	 * 
+	 * /\\[.* \\]\\(http:\\/\\/.* \\)/i
+	 * /\\[.+?\\]\\(http:\\/\\/.+?\\)/i Nicht-leer und nicht gierig
+	 * 
+	 * /\\[.        +?\\]\\(http:\\/\\/.        +?\\)/i
+	 * /\\[[^\\]\\n]+?\\]\\(http:\\/\\/[^\\]\\n]+?\\)/i Keine Klammern und keine Zeilenumbrüche
+	 * 
+	 * /\\[ [^\\]\\n]+? \\]\\( http:\\/\\/[^\\]\\n]+? \\)/i
+	 * /\\[([^\\]\\n]+?)\\]\\((http:\\/\\/[^\\]\\n]+?)\\)/i Gruppen
+	 */
+	$string=preg_replace("/\\[([^\\]\\n]+?)\\]\\((http:\\/\\/[^\\]\\n]+?)\\)/i", "<a href=\"$2\">$1</a>", $string);
+	return $string;
+}
+
+?>
